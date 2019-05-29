8251cef96ee5373652ee3d558a80248f078eea01
丨Rainbow丨 <775183940@qq.com>
2019-05-29 13:48:07 +0800

    新增slide animation  仿网易云音乐打开activity的动画

--- app/src/main/java/com/hyfun/transitionanimation/BottomActivity.java
+++ app/src/main/java/com/hyfun/transitionanimation/BottomActivity.java
@@ -1,13 +1,21 @@
 package com.hyfun.transitionanimation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 public class BottomActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_bottom);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(0, R.anim.anim_slide_close_exit);
     }
 }
